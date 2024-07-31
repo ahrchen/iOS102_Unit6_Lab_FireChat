080b787b0f9adfe609b8ee1e9001e95939f2892a
Raymond Chen <ahrchen@gmail.com>
2024-07-31 13:36:09 -0700

    Initial Commit

---
+++ FireChat/FireChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireChatApp.swift
+//  FireChat
+//
+//  Created by Raymond Chen on 7/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FireChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
